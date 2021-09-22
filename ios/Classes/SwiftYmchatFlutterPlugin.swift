09e4e5e27b629c74c3364e09155f3527611b804c
PurushYM <purushottam.yadav@yellowmessenger.com>
2021-09-22 21:53:02 +0530

    iOS bride events completed

--- ios/Classes/SwiftYmchatFlutterPlugin.swift
+++ ios/Classes/SwiftYmchatFlutterPlugin.swift
@@ -3,9 +3,19 @@ import UIKit
 import YMChat
 
 public class SwiftYmchatFlutterPlugin: NSObject, FlutterPlugin {
-    static var ymConfig:YMConfig? = nil;
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "com.yellow.ai.ymchat", binaryMessenger: registrar.messenger())
+    private static var ymConfig:YMConfig? = nil;
+    
+    private static var ymEventHandler: YmChatFlutterStreamHandler  = YmChatFlutterStreamHandler();
+    
+    private static var ymCloseEventHandler: YmChatFlutterStreamHandler = YmChatFlutterStreamHandler();
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+    
+    let channel = FlutterMethodChannel(name: "com.yellow.ai.ymchat", binaryMessenger: registrar.messenger());
+    
+    let ymEventChannel:FlutterEventChannel = FlutterEventChannel(name:"YMChatEvent",binaryMessenger: registrar.messenger());
+    
+    let ymCloseEventChannel:FlutterEventChannel = FlutterEventChannel(name:"YMBotCloseEvent",binaryMessenger: registrar.messenger());
     
     let instance = SwiftYmchatFlutterPlugin()
     
@@ -16,37 +26,38 @@ public class SwiftYmchatFlutterPlugin: NSObject, FlutterPlugin {
               switch(call.method)
               {
               case "setBotId":
-                self.setBotId(call: call,result: result);
-                  break;
+                self.setBotId(call: call,result: result, ymEventChannel: ymEventChannel, ymCloseEventChannel:ymCloseEventChannel);
+                return;
               case "setDeviceToken":
                   self.setDeviceToken(call: call,result: result);
-                  break;
+                return;
               case "setEnableSpeech":
                   self.setEnableSpeech(call: call,result: result);
-                  break;
+                return;
               case "setEnableHistory":
                   self.setEnableHistory(call: call,result: result);
-                  break;
+                return;
               case "setAuthenticationToken":
                   self.setAuthenticationToken(call:call,result: result)
-                  break;
+                return;
               case "showCloseButton":
                   self.showCloseButton(call:call, result:result);
-                  break;
+                return;
               case "setCustomURL":
                   self.setCustomURL(call:call,result:result);
-                  break;
+                return;
               case "setPayload":
                   self.setPayload(call:call,result:result);
-                  break;
+                return;
               case "startChatbot":
                   self.startChatbot(result:result);
-                  break;
+                return;
               case "closeBot":
                   self.closeBot(result:result);
-                  break;
+                return;
               default:
                   result(FlutterMethodNotImplemented)
+                return;
               }
           // Note: this method is invoked on the UI thread.
           
@@ -54,10 +65,21 @@ public class SwiftYmchatFlutterPlugin: NSObject, FlutterPlugin {
   }
     
     
-    private static func setBotId(call: FlutterMethodCall, result: FlutterResult)
+    private static func setBotId(call: FlutterMethodCall, result: FlutterResult, ymEventChannel: FlutterEventChannel, ymCloseEventChannel: FlutterEventChannel)
     {
         let botId:String = getRequiredParamater(parameter: "botId", call: call)
+        
+        ymEventChannel.setStreamHandler(ymEventHandler);
+        
+        ymCloseEventChannel.setStreamHandler(ymCloseEventHandler);
+        
+        let localDelegate = CustomYmChatDelegate(ymEventHandler: ymEventHandler, ymCloseEventHandler: ymCloseEventHandler)
+        
+        YMChat.shared.delegate = localDelegate;
+        
         ymConfig = YMConfig(botId: botId);
+        
+        
         result(true);
     }
     private static func setDeviceToken(call: FlutterMethodCall, result: FlutterResult)
@@ -86,7 +108,7 @@ public class SwiftYmchatFlutterPlugin: NSObject, FlutterPlugin {
     }
     private static func showCloseButton(call: FlutterMethodCall, result: FlutterResult)
     {
-        let showCloseButton:Bool = getRequiredParamater(parameter: "showCloseButton", call: call)
+        let showCloseButton:Bool = getRequiredParamater(parameter: "shouldEnableCloseButton", call: call)
         ymConfig?.showCloseButton = showCloseButton
         result(true);
     }
@@ -136,4 +158,59 @@ public class SwiftYmchatFlutterPlugin: NSObject, FlutterPlugin {
         }
     }
 
+}
+
+class YmChatFlutterStreamHandler: NSObject,FlutterStreamHandler {
+    
+    var _eventSink: FlutterEventSink?
+    
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        _eventSink = events;
+        return nil;
+    }
+    
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        _eventSink = nil;
+        return nil;
+    }
+    
+    public func getEventSink() -> FlutterEventSink?
+    {
+        return _eventSink;
+    }
+    
+    @objc public func sendEvent(event:Any)
+    {
+        if(_eventSink != nil)
+        {
+            DispatchQueue.main.async {
+                self._eventSink!(event);
+            }
+        }
+        else{
+            assert(false, "Event sink is missing to emmit event");
+        }
+    }
+}
+
+class CustomYmChatDelegate: NSObject,YMChatDelegate{
+    
+    var ymEventHandler: YmChatFlutterStreamHandler?
+    var ymCloseEventHandler: YmChatFlutterStreamHandler?
+    
+    init(ymEventHandler: YmChatFlutterStreamHandler?,ymCloseEventHandler: YmChatFlutterStreamHandler?){
+        self.ymEventHandler = ymEventHandler;
+        self.ymCloseEventHandler = ymCloseEventHandler;
+    }
+    
+    func onEventFromBot(response: YMBotEventResponse) {
+            let event:NSMutableDictionary = NSMutableDictionary();
+            event["code"] = response.code;
+            event["data"] = response.data;
+            ymEventHandler?.sendEvent(event: event);
+    }
+    
+    func onBotClose() {
+        ymCloseEventHandler?.sendEvent(event: true);
+    }
 }
